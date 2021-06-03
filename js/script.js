e0afd5985e38444f0843145c17e830446e5fee43
Yura Luttsev <y.luttsev@gmail.com>
2021-06-03 15:26:02 +0600

    Пробелы версия 2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 'use strict';
-let isNumber = function(n) {
+let isNumber = function(n){
     return !isNaN(parseFloat(n) && isFinite(n));
 };
 
-function guessNumber() {
+function guessNumber(){
 
 let num = prompt('Угадай число от 1 до 100');
 
-   function chcheckingIf() {
-    if ( num > 100) {
+   function chcheckingIf(){
+    if ( num > 100){
         alert('Загаданное число меньше');
         return guessNumber();
-    } else if ( num === null) {
+    } else if ( num === null ){
         alert('Игра окончена');
-    } else if ( !isNumber(num) ) {
+    } else if ( !isNumber(num) ){
         alert('Введи число!');
         return guessNumber();
-    } else if ( num === null) {
+    } else if ( num === null ){
         alert('Игра окончена');
-    } else if (num < 1){
+    } else if ( num < 1 ){
         alert('Загаданное число больше');
         return guessNumber();
     } else {
